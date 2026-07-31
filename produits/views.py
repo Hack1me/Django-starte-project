e85eb50e71f657c270983388dc4f5c3797ac991b
Hack1me <sahitndaheu@gmail.com>
2026-07-31 11:22:41 +0100

    Configuring admin.py

--- produits/views.py
+++ produits/views.py
@@ -1,15 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Produits
-from .forms import ProduitsForm
 
 def produits(request):
-    if request.method == "POST":
-        form = ProduitsForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("/")
-    else:
-        form = ProduitsForm()
-
     myproduits = Produits.objects.all()
-    return render(request, "index.html", context={"mon_produit": myproduits, "form": form})
+    return render(request, "index.html", context={"mon_produit": myproduits,})
